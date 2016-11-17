701914d0575d6e7a384a13c6fe7db2e8be570a55
nanomolina <nanomolinacav@gmail.com>
2016-11-17 13:51:33 -0300

    settings

--- src/sueldo_falcone/sueldo_falcone/settings.py
+++ src/sueldo_falcone/sueldo_falcone/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zbvol9zxzp!ks#c+6!f0qjp_t@g6q#xzx&wrx1@-fbqcrrtg&y'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
