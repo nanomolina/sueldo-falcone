f0d400e94f4bec9bd116974d6ae8d06ee0c832e4
nanomolina <nanomolinacav@gmail.com>
2016-11-17 23:06:18 -0300

    coming soon

---
+++ src/sueldo_falcone/coming_soon/static/coming_soon/js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+	//Code to center the content div
+	$box = $('.content');
+	$ht = $box.height()+175;
+	$win_ht = $(window).height();
+
+	if ($win_ht>$ht) {
+		$box.css({
+			'left' : '50%',
+			'top' : '50%',
+			'margin-left' : -$box.width()/2 + 'px',
+			'margin-top' : -$ht/2 + 'px'
+		});
+	}else{
+		$box.css({
+			'left' : '50%',
+			'margin-left' : -$box.width()/2 + 'px',
+			'margin-top' : '60px',
+			'margin-bottom' : '60px'
+		});
+	}
+
+	//code for the background slider
+	$.backstretch([
+      IMG_1,
+      IMG_2,
+      IMG_3
+    ], {
+        fade: 750,
+        duration: 2500
+    });
+
+ 	//code for the cerlces Countdouwn
+	$(".counter").TimeCircles({
+	    "direction": "Clockwise",
+	    "animation": "Tricks",
+	    "bg_width": 0,
+	    "fg_width": 0.01,
+	    "circle_bg_color": "rgba(255, 255, 255, 0)",
+	    "circle_bg_fill_color": "rgba(255, 255, 255, 0.1)",
+	    "time": {
+	        "Days": {
+	            "text": "Días",
+	            "color": "#ffffff",
+	            "show": true
+	        },
+	        "Hours": {
+	            "text": "Hrs",
+	            "color": "#ffffff",
+	            "show": true
+	        },
+	        "Minutes": {
+	            "text": "Min",
+	            "color": "#ffffff",
+	            "show": true
+	        },
+	        "Seconds": {
+	            "text": "Seg",
+	            "color": "#ffffff",
+	            "show": true
+	        }
+	    }
+	});
+});
--- src/sueldo_falcone/coming_soon/views.py
+++ src/sueldo_falcone/coming_soon/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class InConstruction(TemplateView):
-    template_name = "coming_soon/in_construction.html"
+    template_name = "coming_soon/index.html"
